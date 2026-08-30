c184ddeee08c497847a09fe22167192e6e683276
Wayne <waynegiovanno20@gmail.com>
2026-08-30 08:02:22 +0700

    edit seed.ts

---
+++ prisma/migrations/20260830010044/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isAvailable` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `stock` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "isAvailable",
+DROP COLUMN "stock";
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_username_key" ON "Admin"("username");
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,8 +3,8 @@ import prisma from "../app/lib/prisma";
 async function main() {
   await prisma.admin.create({
     data: {
-      username: "Ayen",
-      password: "1234",
+      username: "ayen",
+      password: "$2b$10$gWLJNEaamcg306chJpqDq.OWiFUTvP9w1cwJwlmXw1u6k9.y65MOG",
     },
   });
 }
