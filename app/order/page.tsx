df5e52b88d954ea17fbd3976b6be86ef1fa4f8d0
Wayne <waynegiovanno20@gmail.com>
2026-09-03 09:28:12 +0700

    update schema.prisma

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Product } from "../types/product";
 
 export default function Order() {
+  const [order, setOrder] = useState<Product[]>([]);
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
@@ -11,6 +12,14 @@ export default function Order() {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  function checkout() {
+    fetch("/api/ordered");
+    setOrder([]);
+  }
+  function addOrder(product: Product) {
+    setOrder((prev) => [...prev, product]);
+  }
 
   return (
     <div className="p-24">
@@ -22,12 +31,19 @@ export default function Order() {
           >
             <p>{product.name}</p>
             <p>{product.price}</p>
-            <button className="bg-[#BC6C25] font-lato rounded-lg text-[#FEFAE0]">
+            <button
+              className="bg-[#BC6C25] font-lato rounded-lg text-[#FEFAE0]"
+              onClick={() => addOrder(product)}
+            >
               + ADD TO ORDER
             </button>
           </div>
         ))}
       </div>
+      {order.map((ordered) => ordered.name)}
+      <button className="bg-green-400 p-4" onClick={checkout}>
+        Checkout Now
+      </button>
     </div>
   );
 }
